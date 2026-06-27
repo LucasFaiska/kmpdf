74648b80cb087d8a59a5181a662c19445bc7f434
Lucas Oliveira <lucas.oliveira@dswiss.com>
2026-06-27 17:25:04 +0100

    chore: bootstrap project using compose multiplatform wizard

---
+++ sharedUI/build.gradle.kts
@@ -0,0 +1,59 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.android.kmp.library)
+}
+
+kotlin {
+    android {
+        namespace = "io.github.lucasfaiska.kmpdf"
+        compileSdk = 36
+        minSdk = 23
+        androidResources.enable = true
+        compilerOptions { jvmTarget = JvmTarget.JVM_17 }
+    }
+
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            api(libs.compose.runtime)
+            api(libs.compose.ui)
+            api(libs.compose.foundation)
+            api(libs.compose.resources)
+            api(libs.compose.ui.tooling.preview)
+            api(libs.compose.material3)
+        }
+
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+            implementation(libs.compose.ui.test)
+        }
+
+        androidMain.dependencies {
+        }
+
+    }
+
+    targets
+        .withType<KotlinNativeTarget>()
+        .matching { it.konanTarget.family.isAppleFamily }
+        .configureEach {
+            binaries {
+                framework {
+                    baseName = "SharedUI"
+                    isStatic = true
+                }
+            }
+        }
+}
+
+dependencies {
+    androidRuntimeClasspath(libs.compose.ui.tooling)
+}
