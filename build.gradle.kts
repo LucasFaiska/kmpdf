e6b448e39ef71729297f465c8753d9b1935f9374
lucas <lucas.faiskaa@gmail.com>
2026-07-19 14:26:30 +0100

    fix: build.gradle.kts ktlint violations and CI tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,26 @@
 plugins {
-    alias(libs.plugins.kotlin.multiplatform).apply(false)
-    alias(libs.plugins.compose.compiler).apply(false)
-    alias(libs.plugins.compose.multiplatform).apply(false)
-    alias(libs.plugins.android.application).apply(false)
-    alias(libs.plugins.android.kmp.library).apply(false)
-    alias(libs.plugins.ktlint).apply(false)
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.compose.multiplatform) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.kmp.library) apply false
+    alias(libs.plugins.ktlint) apply false
     alias(libs.plugins.kover)
     alias(libs.plugins.detekt)
 }
 
-val ktlintId = libs.plugins.ktlint.get().pluginId
-val detektId = libs.plugins.detekt.get().pluginId
-val koverId = libs.plugins.kover.get().pluginId
+val ktlintId =
+    libs.plugins.ktlint
+        .get()
+        .pluginId
+val detektId =
+    libs.plugins.detekt
+        .get()
+        .pluginId
+val koverId =
+    libs.plugins.kover
+        .get()
+        .pluginId
 
 allprojects {
     apply(plugin = ktlintId)
